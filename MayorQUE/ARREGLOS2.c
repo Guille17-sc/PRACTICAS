998cb1d0a5acfdb80c71e52056bd99f2cb509c0a
ASUS MASTER <ASUS MASTER@DESKTOP-JQ17VS5>
2020-11-23 23:23:02 -0600

    23/11/202O array

---
+++ MayorQUE/ARREGLOS2.c
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ARREGLOS2.c
+ * Author: Guille Saldaña 
+ *
+ * Created on 23 de noviembre de 2020, 05:55 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+    int i=0 , x=0, j=0, n=0;
+    int arreglo[n];
+    
+    printf("Ingrese el valor:   ");
+        scanf("%d",&i);
+    for(x;x<i;x++){
+        printf("Ingrese el valor  ");
+        scanf("%d",&arreglo[n]);
+        printf("El valor de %d = %d\n",x = arreglo[n]);
+    }
+        
+        
+    
+    return (EXIT_SUCCESS);
+}
+
